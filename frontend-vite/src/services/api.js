ead848b0bbb5c7bcc91c23023df35a735650a6d3
Lakshithadevi <lakshithadevi2020@gmail.com>
2026-03-31 00:14:23 +0530

    last

--- frontend-vite/src/services/api.js
+++ frontend-vite/src/services/api.js
@@ -2,7 +2,7 @@ const BASE_URL = "https://feedback-issue-tracking-system.onrender.com/api";
 
 // Signup
 export const signup = async (data) => {
-  return fetch(`${BASE_URL}/auth/signup`, {
+  return fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -13,7 +13,7 @@ export const signup = async (data) => {
 
 // Login
 export const login = async (data) => {
-  return fetch(`${BASE_URL}/auth/login`, {
+  return fetch(`${BASE_URL}/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
